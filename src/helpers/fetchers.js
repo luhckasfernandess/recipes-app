94025c7099ec06bf9110b6bde5f10c839acc6c27
Wagner B <wagner_mbarbosa@hotmail.com>
2022-06-21 19:58:35 -0300

    Corrigido erro de "mealsList" em Foods.js retornando como undefined. Foram feitas alterações no fetchers.js para que o requestJson.meals recebesse o mesmo tratamento que o requestJson.drinks

--- src/helpers/fetchers.js
+++ src/helpers/fetchers.js
@@ -1,21 +1,16 @@
-import { API_FOODS_URL, API_COCKTAILS_URL, MAX_LIST_LENGTH } from './constants';
+import { API_COCKTAILS_URL, MAX_LIST_LENGTH } from './constants';
 
 async function getFoodsFromAPI(source, type, query) {
   try {
     // console.log(source);
     const request = await fetch(`${source}${type}${query}`);
     const requestJson = await request.json();
-    if (source === API_FOODS_URL) {
-      if (requestJson[meals].length > MAX_LIST_LENGTH) {
-        return requestJson[meals].slice(0, MAX_LIST_LENGTH);
-      }
-      return requestJson.meals;
-    }
+    let list;
     if (source === API_COCKTAILS_URL) {
-      const list = requestJson.drinks;
-      const slicedList = list.slice(0, MAX_LIST_LENGTH);
-      return slicedList;
-    }
+      list = requestJson.drinks;
+    } else list = requestJson.meals;
+    const slicedList = list.slice(0, MAX_LIST_LENGTH);
+    return slicedList;
   } catch (error) {
     global.alert('Sorry, we haven\'t found any recipes for these filters.');
     // console.log(error);
