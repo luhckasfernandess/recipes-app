d33abf0d75f64583585f116b37a03e320a78df14
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-06-28 16:02:39 -0400

    Implementa solução para que o email do usuário esteja visível

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,28 +3,35 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const userEmail = JSON.parse(localStorage.getItem('user')).email;
   return (
     <div className="profile">
       <Header page="Profile" />
-      <p data-testid="profile-email">email@email.com</p>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-      >
-        Logout
-      </button>
+      <p data-testid="profile-email">{ userEmail }</p>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
