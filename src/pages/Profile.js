e6e9481dd917607027a18f64811afda109fec7f3
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-06-28 16:37:02 -0400

    Implementa solução que redireciona para rota correta ao clicar nos botões

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const history = useHistory();
+
+  // Pega o email da pessoa no localStorage.
   const userEmail = JSON.parse(localStorage.getItem('user')).email;
+
+  // Função de redirecionamento de página ao clicar no botão
+  const redirectBtn = ({ target: { name } }) => {
+    if (name === 'done') history.push('/done-recipes');
+    else if (name === 'favorite') history.push('/favorite-recipes');
+  };
+
   return (
     <div className="profile">
       <Header page="Profile" />
@@ -11,7 +22,9 @@ export default function Profile() {
       <div>
         <button
           type="button"
+          name="done"
           data-testid="profile-done-btn"
+          onClick={ redirectBtn }
         >
           Done Recipes
         </button>
@@ -19,7 +32,9 @@ export default function Profile() {
       <div>
         <button
           type="button"
+          name="favorite"
           data-testid="profile-favorite-btn"
+          onClick={ redirectBtn }
         >
           Favorite Recipes
         </button>
