9d964da5798617896a9f302249be1dae09bd04a2
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-06-28 15:44:24 -0400

    Implementa os elementos da tela de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,25 @@ export default function Profile() {
   return (
     <div className="profile">
       <Header page="Profile" />
+      <p data-testid="profile-email">email@email.com</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
