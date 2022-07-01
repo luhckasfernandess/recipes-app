9b21c25d5adab3c53421931dcb26c6941ba91935
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-07-01 18:39:26 -0300

    conclui funcionalidades da pagina Explore Foods e Explore Drinks

--- src/pages/ExploreThis.js
+++ src/pages/ExploreThis.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+import { API_FOODS_URL, API_COCKTAILS_URL } from '../helpers/constants';
+import getFoodsFromAPI from '../helpers/fetchers';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -7,24 +9,27 @@ export default function ExploreThis() {
   const history = useHistory();
 
   const [type, setType] = useState('');
+  const [randomId, setRandomId] = useState('');
 
   useEffect(() => {
-    if (history.location.pathname.includes('foods')) {
-      return setType('foods');
-    }
-    return setType('drinks');
+    const fetchApi = async () => {
+      const typeee = history.location.pathname.includes('foods') ? 'foods' : 'drinks';
+      setType(typeee);
+      const endpoint = typeee === 'foods' ? API_FOODS_URL : API_COCKTAILS_URL;
+      const idByType = typeee === 'drinks' ? 'idDrink' : 'idMeal';
+      const apiResponse = await getFoodsFromAPI(endpoint, 'random.php', '');
+      console.log(apiResponse[0]);
+      setRandomId(apiResponse[0][idByType]);
+    };
+    fetchApi();
   }, []);
 
-  const handleClick = (where) => {
-    switch (where) {
-    case 'ingredients':
-      return history.push(`/explore/${type}/ingredients`);
-    case 'nationalities':
-      return history.push(`/explore/${type}/nationalities`);
-    default:
-      return console.log(type);
-    }
-  };
+  // const handleClick = () => {
+  //   // history.push(`/${type}/${randomId}`);
+  //   const randomRecipeUrl = `/${type}/${randomId}`;
+  //   console.log(randomRecipeUrl);
+  //   history.push(randomRecipeUrl);
+  // };
 
   return (
     <div className={ `explore-${type}` }>
@@ -46,14 +51,13 @@ export default function ExploreThis() {
             </div>
           </Link>
         )}
-      <div className="explore-by-ingredient" data-testid="explore-surprise">
-        <button
-          type="button"
-          onClick={ () => handleClick() }
-        >
-          Surprise me!
-        </button>
-      </div>
+      <Link to={ `/${type}/${randomId}` }>
+        <div className="explore-by-ingredient" data-testid="explore-surprise">
+          <p>
+            Surprise me!
+          </p>
+        </div>
+      </Link>
       <Footer />
     </div>
   );
